545a726bb64ddc547947aa7e4cb3ff3aa604fb7e
andreistr21 <andstra45@gmail.com>
2023-08-24 20:34:27 +0200

    Improved code quality code Added type hints, cleared code

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,16 +1,16 @@
 import json
+from typing import Any, Dict, List
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from channels.layers import InMemoryChannelLayer
-from django.contrib.auth import get_user_model
 
 from chat.models import Message
-from chat.selectors import last_20_messages
+from chat.selectors import get_author, last_20_messages
 
 
 class ChatConsumer(WebsocketConsumer):
-    def fetch_messages(self, data):
+    def fetch_messages(self, _) -> None:
         """Fetches last 20 messages form database and send to the group"""
         messages = last_20_messages()
         content = {
@@ -19,10 +19,10 @@ class ChatConsumer(WebsocketConsumer):
         }
         self.send_message(content)
 
-    def new_message(self, data):
+    def new_message(self, data: Dict[str, str]) -> None:
         """Saves new message to database and send to room group"""
         author = data["from"]
-        author_user = get_user_model().objects.get(username=author)
+        author_user = get_author(author)
         message = Message.objects.create(
             author=author_user,
             content=data["message"],
@@ -34,11 +34,13 @@ class ChatConsumer(WebsocketConsumer):
         }
         return self.send_chat_message(content)
 
-    def messages_to_json(self, messages):
+    def messages_to_json(
+        self, messages: List[Message]
+    ) -> List[Dict[str, str]]:
         """Serializes messages to JSON"""
         return [self.message_to_json(message) for message in messages]
 
-    def message_to_json(self, message: Message):
+    def message_to_json(self, message: Message) -> Dict[str, str]:
         """Serializes a message to JSON"""
         return {
             "author": message.author.username,
@@ -51,7 +53,7 @@ class ChatConsumer(WebsocketConsumer):
         "new_message": new_message,
     }
 
-    def connect(self):
+    def connect(self) -> None:
         """Joins room group"""
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
@@ -63,19 +65,19 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
-    def disconnect(self, close_code):
+    def disconnect(self, _):
         """Leaves room group"""
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
-    def receive(self, text_data):
+    def receive(self, text_data: str) -> None:
         """Receives message from WebSocket"""
         data = json.loads(text_data)
         func = self.commands[data["command"]]
         func(self, data)
 
-    def send_chat_message(self, message):
+    def send_chat_message(self, message: Dict[str, str]) -> None:
         """Sends message to room group"""
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -85,11 +87,15 @@ class ChatConsumer(WebsocketConsumer):
             },
         )
 
-    def send_message(self, message):
-        """Sends message to current user"""
+    def send_message(
+        self, message: Dict
+    ) -> None:
+        """Sends message to current user (WebSocket)"""
         self.send(text_data=json.dumps(message))
 
-    def chat_message(self, event):
+    def chat_message(
+        self, event: Dict
+    ):
         """Receives message from room group"""
         message = event["message"]
 
--- src/chat/selectors.py
+++ src/chat/selectors.py
@@ -1,6 +1,13 @@
+from typing import List
 from chat.models import Message
 from django.db.models import QuerySet
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
-def last_20_messages() -> QuerySet[Message]:
-    return Message.objects.order_by("-timestamp").all()[:20][::-1]
+def last_20_messages() -> List[Message]:
+    return Message.objects.order_by("-timestamp").all()[:20][::-1]  # type: ignore
+
+
+def get_author(username: str) -> User:
+    return get_user_model().objects.get(username=username)  # type: ignore
