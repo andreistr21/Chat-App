ed1b8db05de41562dfbb23f620ee161fb6a405d1
andreistr21 <andstra45@gmail.com>
2023-08-30 12:40:10 +0200

    Added redirect from main page to chats list

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = i18n_patterns(
     path("chat/", include("chat.urls")),
+    path("", RedirectView.as_view(pattern_name="chat:index")),
     path("admin/", admin.site.urls),
 )
