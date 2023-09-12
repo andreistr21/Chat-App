51b443d209ec6e87a95b576de6ca21a87aa2e08f
andreistr21 <andstra45@gmail.com>
2023-09-12 19:08:22 +0200

    Changed redis key name for users channels names

--- src/chat/utils.py
+++ src/chat/utils.py
@@ -2,4 +2,4 @@ def construct_name_of_redis_list_for_channel_name(user_id: str) -> str:
     """
     Construct name for redis list that contains django-channels name.
     """
-    return f"asgi:channels_name:{user_id}"
+    return f"asgi:users_channels_names:{user_id}"
