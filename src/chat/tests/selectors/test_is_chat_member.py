d0bae72f9a0b2c26f2eec58c40341afc77225e1a
andreistr21 <andstra45@gmail.com>
2023-10-05 15:58:07 +0200

    Updated tests for is chat member selector

--- src/chat/tests/selectors/test_is_chat_member.py
+++ src/chat/tests/selectors/test_is_chat_member.py
@@ -1,38 +1,90 @@
 import pytest
 from django.contrib.auth.models import User
+from pytest_mock import MockerFixture
 
 from chat.models import ChatRoom
 from chat.selectors import is_chat_member
 from chat.tests.services import multiple_users_generator
 
 
-@pytest.mark.django_db
-@pytest.mark.parametrize("num_members", (1, 2, 3))
-def test_if_user_is_member(room_no_members: ChatRoom, num_members: int):
+def _add_users_to_room(num_members: int, room: ChatRoom) -> list[User]:
     multiple_users = multiple_users_generator()
     users = []
     for _ in range(num_members):
         user = next(multiple_users)
-        room_no_members.members.add(user)
+        room.members.add(user)
         users.append(user)
 
-    is_member = is_chat_member(users[0].id, room_no_members)
+    return users
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "num_members",
+    (1, 2, 3),
+)
+def test_if_room_exists_user_is_member(
+    mocker: MockerFixture,
+    room_no_members: ChatRoom,
+    num_members: int,
+):
+    mocker.patch("chat.selectors.get_room", return_value=room_no_members)
+
+    users = _add_users_to_room(num_members, room_no_members)
+
+    is_member = is_chat_member(users[0].id, str(room_no_members.id))
 
     assert is_member
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("num_members", (0, 1, 2, 3))
-def test_if_user_is_not_member(
-    user: User, room_no_members: ChatRoom, num_members: int
+@pytest.mark.parametrize(
+    "num_members",
+    (1, 2, 3),
+)
+def test_if_room_doesnt_exists_user_is_member(
+    mocker: MockerFixture,
+    room_no_members: ChatRoom,
+    num_members: int,
 ):
-    multiple_users = multiple_users_generator()
-    users = []
-    for _ in range(num_members):
-        temp_user = next(multiple_users)
-        room_no_members.members.add(temp_user)
-        users.append(temp_user)
+    mocker.patch("chat.selectors.get_room", return_value=None)
 
-    is_member = is_chat_member(user.id, room_no_members)
+    users = _add_users_to_room(num_members, room_no_members)
+
+    is_member = is_chat_member(users[0].id, str(room_no_members.id))
+
+    assert not is_member
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("num_members", (0, 1, 2, 3))
+def test_if_room_exists_user_is_not_member(
+    mocker: MockerFixture,
+    user: User,
+    room_no_members: ChatRoom,
+    num_members: int,
+):
+    mocker.patch("chat.selectors.get_room", return_value=room_no_members)
+
+    _add_users_to_room(num_members, room_no_members)
+
+    is_member = is_chat_member(user.id, str(room_no_members.id))
+
+    assert not is_member
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("num_members", (0, 1, 2, 3))
+def test_if_room_doesnt_exists_user_is_not_member(
+    mocker: MockerFixture,
+    user: User,
+    room_no_members: ChatRoom,
+    num_members: int,
+):
+    mocker.patch("chat.selectors.get_room", return_value=None)
+
+    _add_users_to_room(num_members, room_no_members)
+
+    is_member = is_chat_member(user.id, str(room_no_members.id))
 
     assert not is_member
