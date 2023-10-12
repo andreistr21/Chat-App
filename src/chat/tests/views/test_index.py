d45d15807fd7b61f86bd37bc212e622ec7e31ad7
andreistr21 <andstra45@gmail.com>
2023-10-12 13:32:20 +0200

    Improved test for index view

--- src/chat/tests/views/test_index.py
+++ src/chat/tests/views/test_index.py
@@ -2,15 +2,19 @@ import pytest
 from django.contrib.auth.models import AnonymousUser, User
 from django.test.client import RequestFactory
 from django.urls import reverse
+from pytest_mock import MockerFixture
 
 from chat.views import index
 
 
 @pytest.mark.django_db
-def test_access_allowed(rf: RequestFactory, user: User):
+def test_access_allowed(rf: RequestFactory, mocker: MockerFixture, user: User):
     """
     Tests index view with authenticated user.
     """
+    mocker.patch("chat.views.get_user_chats")
+    mocker.patch("chat.views.chats_list")
+
     request = rf.get(reverse("chat:index"))
     request.user = user
 
