f82e30a007fa84933c93ec3d7111bfeac5bc0e5e
andreistr21 <andstra45@gmail.com>
2023-10-12 20:08:40 +0200

    Added ability to register

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
+from users.views import RegistrationView
+
 app_name = "users"
 urlpatterns = [
     path(
         "login",
         LoginView.as_view(template_name="users/login.html"),
         name="login",
+    ),
+    path(
+        "register",
+        RegistrationView.as_view(),
+        name="register",
     ),
 ]
